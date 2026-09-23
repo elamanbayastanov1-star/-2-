3a6f4c02f23ab69ea5d53e2d5b6b3a2e1b07fdfb
elamanbayastanov1-star <elamanbayastanov1@gmail.com>
2026-09-24 00:12:08 +0600

    Add files via upload

---
+++ homework 3.py
@@ -0,0 +1,54 @@
+
+from abc import ABC, abstractmethod
+
+
+class Hero(ABC):
+    def __init__(self, name, level, health, strength):
+        self.name = name
+        self.level = level
+        self.__health = health
+        self.strength = strength
+
+    def greet(self):
+        print(f"Привет, я {self.name}, мой уровень {self.level}")
+
+    def rest(self):
+        print(f"{self.name} отдыхает")
+        self.__health += 1
+
+    @abstractmethod
+    def attack(self):
+        pass
+
+
+class Warrior(Hero):
+    def attack(self):
+        print("Воин атакует мечом")
+
+
+class Mage(Hero):
+    def attack(self):
+        print("Маг использует магию")
+
+
+class Assassin(Hero):
+    def attack(self):
+        print("Ассасин атакует из-под тишка")
+
+
+warrior = Warrior("Артур", 5, 100, 20)
+mage = Mage("Маг", 7, 80, 15)
+assassin = Assassin("Ниндзя", 6, 90, 25)
+
+
+warrior.greet()
+warrior.attack()
+warrior.rest()
+
+mage.greet()
+mage.attack()
+mage.rest()
+
+assassin.greet()
+assassin.attack()
+assassin.rest()
